e349c757da2d767984385d7620a405c378300185
HHieu2003 <hieutran170626@gmail.com>
2026-04-28 00:58:27 +0700

    loadpage

--- resources/views/frontend/partials/page-loader.blade.php
+++ resources/views/frontend/partials/page-loader.blade.php
@@ -1,7 +1,12 @@
 <style>
     #page-loader {
         position: fixed;
-        inset: 0;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        width: 100%;
+        height: 100%;
         z-index: 9999;
         background: #fff;
         display: flex;
